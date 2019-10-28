ea3e5a44ae91ef8fc40d354636e005f21da02b48
Sushi6006 <lin.terry.x2@gmail.com>
2019-10-28 15:12:12 +1100

    rearraged files

--- one_to_fifty/one_to_fifty.py
+++ one_to_fifty/one_to_fifty.py
@@ -1,6 +1,13 @@
 # game_bot for 1to50
+# completed by Sushi6006 on 28 Oct. 2019
+# code available at https://github.com/Sushi6006/game_bots/blob/master/one_to_fifty.py
+
 # a chinese online game that requires you to click 1 to 50 sequentially
-# link: http://wap.jue-huo.com/app/html/game/1to50/1to50.html
+# link to the game: http://wap.jue-huo.com/app/html/game/1to50/1to50.html
+# Developed in Python 3.7 (anaconda) with Selenium in Atom
+
+# xpath cheat sheet: https://devhints.io/xpath
+# chrome driver download: https://chromedriver.storage.googleapis.com/index.html
 
 from selenium import webdriver
 from time import sleep
