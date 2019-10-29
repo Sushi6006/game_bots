42ea63d60c468fbcdbbcefed3744d3800de717b7
Sushi6006 <lin.terry.x2@gmail.com>
2019-10-29 12:11:21 +1100

    info update in 1to50

--- one_to_fifty/one_to_fifty.py
+++ one_to_fifty/one_to_fifty.py
@@ -1,6 +1,6 @@
 # game_bot for 1to50
 # completed by Sushi6006 on 28 Oct. 2019
-# code available at https://github.com/Sushi6006/game_bots/blob/master/one_to_fifty.py
+# code available at https://github.com/Sushi6006/game_bots/blob/master/one_to_fifty/one_to_fifty.py
 
 # a chinese online game that requires you to click 1 to 50 sequentially
 # link to the game: http://wap.jue-huo.com/app/html/game/1to50/1to50.html
@@ -8,6 +8,8 @@
 
 # xpath cheat sheet: https://devhints.io/xpath
 # chrome driver download: https://chromedriver.storage.googleapis.com/index.html
+
+# chinese tutorial: https://mp.weixin.qq.com/s?__biz=MzU2NTgyOTc0Ng==&mid=2247483672&idx=1&sn=4c9a98ede1121d8e2d5b10b3f38e6264&chksm=fcb48805cbc301136ba09245c9a2632a2735003cbb1cffcebf8ca3350a5fc1bb13a5230d4e71
 
 from selenium import webdriver
 from time import sleep
